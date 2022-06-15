8a331e158734fd37ce1e686c15cbf137636273ad
aprilkrg <aprilkrgonzales@gmail.com>
2022-06-15 13:07:58 -0600

    inline check for service

--- src/components/pages/Service.js
+++ src/components/pages/Service.js
@@ -4,15 +4,16 @@ const Service = (props) => {
     let { id } = useParams()
     const service = props.services.find(service => service.id.toString() === id)
     // if service.id is NOT in the props.services arr then return text saying it doesn't exist
-    const idsArr = props.services.map(service => {
-        return service.id})
-    console.log(idsArr, id, "idssss")
-    if(idsArr.includes(parseInt(id))) {
-        console.log("true")
-    } else {
-        console.log("false")
-        return <h1>Dont' have that service</h1>
-    }
+    // const idsArr = props.services.map(service => {
+    //     return service.id})
+    // console.log(idsArr, id, "idssss")
+    // if(idsArr.includes(parseInt(id))) {
+    //     console.log("true")
+    // } else {
+    //     console.log("false")
+    //     return <h1>Dont' have that service</h1>
+    // }
+    if(!service) return <h1>Dont' have that service</h1>
     return (
         <>
             <h3>{service.name}</h3>
